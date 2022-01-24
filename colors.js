c5d34a62833a66b44c550dbb04898e69e2383dbb
leejuy1140 <leejuy1140@gmail.com>
2022-01-25 01:31:17 +0900

    apply jQuery

--- colors.js
+++ colors.js
@@ -1,19 +1,23 @@
 var Links = {
     setColor : function (color) {
-        var i = 0;
+        /*var i = 0;
         var alist = document.querySelectorAll('a');
         while (i < alist.length) {
             alist[i].style.color = color;
             i = i + 1;
-        }
+        }*/
+        //jQuery 적용: 모든 a태그의 css를 바꾸는 것을 jQuery에서 제어
+        $('a').css('color', color);
     }
 }
 var Body = {
     setColor : function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor : function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 function nightDayHandler(myself) {
